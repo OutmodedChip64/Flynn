7ee57e940c661f4c8ae8dd8ef42bb5de6f72f0f8
OutmodedChip64 <cherryman656@gmail.com>
2019-05-05 18:29:14 -0700

    Add project files.

---
+++ Flynn/Basics.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+namespace Flynn
+{
+    public class Basics
+    {
+        public void RepeatFunc(int times, string name)
+        {
+            int i;
+            for (i = 0; i < times; i++)
+            {
+                Type thisType = this.GetType();
+                MethodInfo theMethod = thisType.GetMethod(name);
+                theMethod.Invoke(this, null);
+            }
+        }
+    }
+}
