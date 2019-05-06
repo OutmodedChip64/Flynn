98d35556b07a57c0c94becfc2984db37dea54593
OutmodedChip64 <cherryman656@gmail.com>
2019-05-05 18:51:05 -0700

    Changed name to funcName

--- Flynn/Basics.cs
+++ Flynn/Basics.cs
@@ -5,13 +5,13 @@ namespace Flynn
 {
     public class Basics
     {
-        public void RepeatFunc(int times, string name)
+        public void RepeatFunc(int times, string funcName)
         {
             int i;
             for (i = 0; i < times; i++)
             {
                 Type thisType = this.GetType();
-                MethodInfo theMethod = thisType.GetMethod(name);
+                MethodInfo theMethod = thisType.GetMethod(funcName);
                 theMethod.Invoke(this, null);
             }
         }
